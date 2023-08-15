5f433441647360faf02a48a68eae91b3cd17cc03
muaazshoaib <muaazshoaib1670@gmail.com>
2023-08-16 01:54:26 +0500

    Requesting Permissions & Getting an Address Token

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,16 +1,42 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_app/widgets/chat_messages.dart';
 import 'package:flutter_chat_app/widgets/new_message.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void setupPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+    final token = await fcm.getToken();
+
+    if (kDebugMode) {
+      print(
+          'Token: $token'); // you could send this token (via HTTP or the Firestore SDK) to a backend
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    setupPushNotifications();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('FlutterChat'),centerTitle: true,
+        title: const Text('FlutterChat'),
+        centerTitle: true,
         actions: [
           IconButton(
             icon: Icon(
